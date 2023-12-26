ff27dc9e9aa3bab9ea56736bc7140b541d1ff42d
ayush391 <stringakaak@gmail.com>
2023-12-26 11:25:58 +0530

    initial commit

---
+++ delmerged.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Specify branches to exclude
+EXCLUDE_BRANCHES="(^\\*|master|main|development)"
+
+# List local branches that are merged and exclude specified branches
+MERGED_BRANCHES=$(git branch --merged | egrep -v "$EXCLUDE_BRANCHES")
+
+# Check if there are any branches to delete
+if [ -z "$MERGED_BRANCHES" ]; then
+  echo "No merged local branches to delete."
+  exit 0
+fi
+
+echo "Deleting merged local branches:"
+
+# Delete each merged local branch
+echo "$MERGED_BRANCHES" | xargs -n 1 git branch -d
+
+echo "Deletion complete."
